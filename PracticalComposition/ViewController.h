07a3ff2277368f55c98b0277ec2d44291a6a5ee0
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2015-01-14 19:33:09 +0200

    Initial Commit

---
+++ PracticalComposition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PracticalComposition
+//
+//  Created by Georgi Ivanov on 1/14/15.
+//  Copyright (c) 2015 GeorgiIvanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
